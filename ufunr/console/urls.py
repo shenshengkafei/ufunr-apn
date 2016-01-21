998a898589b85038318373bad034e0bfc3c7c14f
shenshengkafei <wudiadi1111@gmail.com>
2016-01-21 03:41:05 -0800

    fixed a typo that ufunr now supports updating with negative data

--- ufunr/console/urls.py
+++ ufunr/console/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^validuser/$', views.validuser, name='validuser'),
     url(r'^expireduser/$', views.expireduser, name='expireduser'),
-    url(r'^(?P<account_id>[0-9]+)/(?P<valid_days>[0-9]+)/(?P<data_left>[0-9]+\.[0-9]+)/(?P<is_deleted>[A-Z]+)/updateuserinfo$', views.updateuserinfo, name='updateuserinfo'),
+    url(r'^(?P<account_id>[0-9]+)/(?P<valid_days>[0-9]+)/(?P<data_left>[-]?[0-9]+\.[0-9]+)/(?P<is_deleted>[A-Z]+)/updateuserinfo$', views.updateuserinfo, name='updateuserinfo'),
 ]
